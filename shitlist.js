285c5b7b5e7b8c80d6191f38b0f01cc0ef99cab5
Sven Poppelmann <github@kalayl.net>
2013-01-24 00:07:23 +0000

    added backbone and a splash page, but have a bug with list creation

--- shitlist.js
+++ shitlist.js
@@ -1,19 +1,80 @@
 Offences = new Meteor.Collection("offences");
+Shitlists = new Meteor.Collection("shitlists");
 
 if (Meteor.isClient) {
-    Template.leaderboard.shitlist = function () {
-        return this.window.location.pathname.substr(1);
+    var Router = Backbone.Router.extend({
+        routes: {
+            "" : "main",
+            ":page": "main" //this will be http://your_domain/
+        },
+        main: function (page) {
+            document.body.innerHTML = "";
+            page = page ? "shitlist" : "index";
+
+            var frag = Meteor.render(function () {
+                var i = Template[page] ? Template[page]() : "";
+                return i;
+            });
+
+            document.body.appendChild(frag);
+        }
+    });
+
+    var app = new Router;
+
+    Meteor.startup(function () {
+        Backbone.history.start({pushState: true});
+    });
+
+    Template.index.shitlists = function () {
+        return Shitlists.find({}, { sort: { creationDate: -1 }});
     };
 
-    Template.leaderboard.offences = function () {
-        var shitlist = this.window.location.pathname.substr(1);
-        return Offences.find({shitlist: shitlist}, { sort: { severity: -1, offenceDate: -1 } });
+    Template.shitlistItem.listUri = function () {
+        return window.location + this.name;
     };
 
-    Template.leaderboard.events({
+    Template.index.events({
+        'keypress #newShitlist': function (event, template) {
+            if (event.keyCode === 13) {
+                var newShitlistName = this.newShitlist.value;
+                window.location.replace(window.location + newShitlistName);
+            }
+        }
+    });
+
+    var checkAndCreateNewShitlist = function (newShitlistName) {
+        var list = Shitlists.findOne({ name: newShitlistName });
+        console.log(newShitlistName + " = " + list);
+        if (list === undefined) {
+            console.log('creating a new list ' + newShitlistName);
+            Shitlists.insert({
+                name: newShitlistName,
+                creationDate: new Date()
+            });
+        }
+    };
+
+    Template.shitlist.shitlistUri = function () {
+        return window.location.pathname.substr(1);
+    };
+
+    Template.shitlist.created = function () {
+        var shitlist = window.location.pathname.substr(1);
+        checkAndCreateNewShitlist(shitlist);
+    };
+
+    Template.shitlist.offences = function () {
+        var shitlist = window.location.pathname.substr(1);
+        if (shitlist.length) {
+            return Offences.find({shitlist: shitlist}, { sort: { severity: -1, offenceDate: -1 } });
+        }
+    };
+
+    Template.shitlist.events({
         'keypress #offender': function (event, template) {
             if (event.keyCode === 13) {
-                var shitlist = this.window.location.pathname.substr(1);
+                var shitlist = window.location.pathname.substr(1);
                 var offenceId = Offences.insert({
                     shitlist: shitlist,
                     reason: 'your reason',
@@ -23,17 +84,15 @@ if (Meteor.isClient) {
                     severity: 5,
                     done: false
                 });
-                Session.set("selected_offender", offenceId);
+                //Session.set("selected_offender", offenceId);
                 this.offender.value = '';
             }
-        }//,
-//        'click #add-new-offence': function() {
-//        }
+        }
     });
 
-    Template.offence.selected = function () {
-        return Session.equals("selected_offender", this._id);
-    };
+    //Template.offence.selected = function () {
+    //    return Session.equals("selected_offender", this._id);
+    //};
 
     Template.offence.offenceHandled = function () {
         return this.done ? "checked" : "";
@@ -46,6 +105,28 @@ if (Meteor.isClient) {
     Template.offence.formattedOffenceDate = function () {
         return (this.offenceDate ? new Date(this.offenceDate).toDateString() : "");
     };
+
+    Template.offence.events({
+        'click .readonly': function (event, template) {
+            startEdit(template, readOnlyFieldName(event));
+        },
+        'blur .editable': function (event, template) {
+            endEdit(template, editableFieldName(event));
+        },
+
+        'click #remove-offence': function (event, template) {
+            Offences.remove(template.data._id);
+        },
+        'click #decrease-severity': function (event, template) {
+            Offences.update({ _id: template.data._id, severity: { $gt: 1} }, { $inc: { severity: -1 }}, { multi: false });
+        },
+        'click #increase-severity': function (event, template) {
+            Offences.update({ _id: template.data._id, severity: { $lt: 5} }, { $inc: { severity: 1 }}, { multi: false });
+        },
+        'click #offender-handled': function (event, template) {
+            Offences.update({ _id: template.data._id}, { $set: { done: !template.data.done } }, { multi: false });
+        }
+    });
 
     var startEdit = function (template, field) {
         console.log('startedit');
@@ -94,35 +175,18 @@ if (Meteor.isClient) {
         });
         return result;
     };
-
-    Template.offence.events({
-        'click .readonly': function (event, template) {
-            startEdit(template, readOnlyFieldName(event));
-        },
-        'blur .editable': function (event, template) {
-            endEdit(template, editableFieldName(event));
-        },
-
-        'click #remove-offence': function (event, template) {
-            Offences.remove(template.data._id);
-        },
-        'click #decrease-severity': function (event, template) {
-            Offences.update({ _id: template.data._id, severity: { $gt: 1} }, { $inc: { severity: -1 }}, { multi: false });
-        },
-        'click #increase-severity': function (event, template) {
-            Offences.update({ _id: template.data._id, severity: { $lt: 5} }, { $inc: { severity: 1 }}, { multi: false });
-        },
-        'click #offender-handled': function (event, template) {
-            Offences.update({ _id: template.data._id}, { $set: { done: !template.data.done } }, { multi: false });
-        }
-    });
 }
 
 if (Meteor.isServer) {
     Meteor.startup(function () {
         if (Offences.find().count() === 0) {
+            Shitlists.insert({
+                name: '123',
+                creationDate: new Date()
+            });
+
             Offences.insert({
-                shitlist: "123",
+                shitlist: '123',
                 offender: 'Joe Bloggs',
                 offenceDate: new Date(),
                 severity: 1,
@@ -132,7 +196,7 @@ if (Meteor.isServer) {
             });
 
             Offences.insert({
-                shitlist: "123",
+                shitlist: '123',
                 offender: 'Mary Jane',
                 offenceDate: new Date(),
                 severity: 5,
